760c00aa6006d8190f3b3b664b8a4e371047e2a6
athenova <aleksey.sergeyev@yandex.com>
2025-03-25 22:53:50 +0300

    Постер в ВК

--- project.py
+++ project.py
@@ -1,14 +1,18 @@
 from simple_blogger import Teacher
 from datetime import datetime
+from simple_blogger.senders.TelegramSender import TelegramSender
+from simple_blogger.senders.VkSender import VkSender
 
 class Project(Teacher):
     def __init__(self, **kwargs):
         super().__init__(            
-            review_chat_id=-1002374309134,
             first_post_date=datetime(2025, 1, 6),
             send_text_with_image=True,
             topic_word_limit=200,
             last_post_date=datetime(2025, 5, 20),
+            reviewer=TelegramSender(),
+            senders=[TelegramSender(channel_id=f"@class5nik"), 
+                     VkSender(group_id='229821544')],
             **kwargs)
         
     def _system_prompt(self, _):
@@ -22,5 +26,3 @@ class Project(Teacher):
                 "topic_image": f"Нарисуй картинку, вдохновлённую темой '{topic}' из '{idea['category']}'",
                 "topic_prompt": f"Напиши интересный факт по теме '{topic}' из '{idea['category']}', используй менее {self.topic_word_limit} слов",
             }
-    
-   
